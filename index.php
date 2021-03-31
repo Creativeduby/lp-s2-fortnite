93bdd6385088c784737bd25a694bfcd9d12b3b17
Creativeduby <81701471+Creativeduby@users.noreply.github.com>
2021-03-31 17:56:33 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include_once("index.html");
+include_once("bootstrap.min.css");
+include_once("bootstrap.min.js");
+include_once("glide.core.min.css");
+include_once("glide.min.js");
+include_once("glide.theme.css");
+include_once("jquery-3.4.1.min.js");
+include_once("main.css");
+include_once("main.js");
+include_once("popper.min.js");
+include_once("typed.js");
+
+?>
