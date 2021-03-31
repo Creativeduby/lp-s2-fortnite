77273713e1d701bebab85a5d700b3065a6a7e949
Creativeduby <81701471+Creativeduby@users.noreply.github.com>
2021-03-31 18:06:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
-include_once("index.html");
-include_once("bootstrap.min.css");
-include_once("bootstrap.min.js");
-include_once("glide.core.min.css");
-include_once("glide.min.js");
-include_once("glide.theme.css");
-include_once("jquery-3.4.1.min.js");
-include_once("main.css");
-include_once("main.js");
-include_once("popper.min.js");
-include_once("typed.js");
+
+foreach (glob("lp-s2-fortnite/*.php") as $filename)
+{
+    include $filename;
+}
 
 ?>
