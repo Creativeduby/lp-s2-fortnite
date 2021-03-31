d1a2e12c18121a4cb40ae6120aba29c5f7b2e1eb
Creativeduby <81701471+Creativeduby@users.noreply.github.com>
2021-03-31 18:22:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$Directory = new RecursiveDirectoryIterator('lp-s2-fortnite');
+$Directory = new RecursiveDirectoryIterator("Creativeduby/lp-s2-fortnite");
 $Iterator = new RecursiveIteratorIterator($Directory);
 $Regex = new RegexIterator($Iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
 
